b565bd2176c5cafd51b265c2a2ccbe8de7f219ba
Arno <Arno@DESKTOP-QGAOB2C>
2020-04-05 17:00:51 +0200

    Get Set

--- Actis/src/application/Acti.java
+++ Actis/src/application/Acti.java
@@ -6,5 +6,36 @@ private int igy;
   private int harom;
   private int negy;
   private String ot;
+public int getIgy() {
+	return igy;
+}
+public void setIgy(int igy) {
+	this.igy = igy;
+}
+public int getKetto() {
+	return ketto;
+}
+public void setKetto(int ketto) {
+	this.ketto = ketto;
+}
+public int getHarom() {
+	return harom;
+}
+public void setHarom(int harom) {
+	this.harom = harom;
+}
+public int getNegy() {
+	return negy;
+}
+public void setNegy(int negy) {
+	this.negy = negy;
+}
+public String getOt() {
+	return ot;
+}
+public void setOt(String ot) {
+	this.ot = ot;
+}
+  
 
 }
