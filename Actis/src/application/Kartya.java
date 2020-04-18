4812a39e1cb713e37eebd21927c57d413f90d385
Arno <Arno@DESKTOP-QGAOB2C>
2020-04-18 14:01:48 +0200

    Kartya  objektum

---
+++ Actis/src/application/Kartya.java
@@ -0,0 +1,69 @@
+package application;
+
+public class Kartya {
+	private String mutogat1;
+	private String beszel1;
+	private String rajz1;
+	private String mutogat2;
+	private String beszel2;
+	private String rajz2;
+	
+	public Kartya(String mutogat1, String beszel1, String rajz1, String mutogat2, String beszel2, String rajz2) {
+		this.mutogat1 = mutogat1;
+		this.beszel1 = beszel1;
+		this.rajz1 = rajz1;
+		this.mutogat2 = mutogat2;
+		this.beszel2 = beszel2;
+		this.rajz2 = rajz2;
+	}
+
+	public String getMutogat1() {
+		return mutogat1;
+	}
+
+	public void setMutogat1(String mutogat1) {
+		this.mutogat1 = mutogat1;
+	}
+
+	public String getBeszel1() {
+		return beszel1;
+	}
+
+	public void setBeszel1(String beszel1) {
+		this.beszel1 = beszel1;
+	}
+
+	public String getRajz1() {
+		return rajz1;
+	}
+
+	public void setRajz1(String rajz1) {
+		this.rajz1 = rajz1;
+	}
+
+	public String getMutogat2() {
+		return mutogat2;
+	}
+
+	public void setMutogat2(String mutogat2) {
+		this.mutogat2 = mutogat2;
+	}
+
+	public String getBeszel2() {
+		return beszel2;
+	}
+
+	public void setBeszel2(String beszel2) {
+		this.beszel2 = beszel2;
+	}
+
+	public String getRajz2() {
+		return rajz2;
+	}
+
+	public void setRajz2(String rajz2) {
+		this.rajz2 = rajz2;
+	}
+	
+	
+}
--- Actis/src/application/Main.java
+++ Actis/src/application/Main.java
@@ -24,6 +24,7 @@ public class Main /*extends Application*/ {
 		//launch(args);
 		Acti acti = new Acti();
 		System.out.println(acti.randomnum());
+		System.out.println();
 		
 		
 	}
