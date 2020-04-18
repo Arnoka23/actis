b7277456e5cf032dbf36feb8937bfe2a7e32c732
steik <kozmann66@gmail.com>
2020-04-18 11:59:03 +0200

    feladatok hozzáadva

--- Actis/src/application/Acti.java
+++ Actis/src/application/Acti.java
@@ -1,6 +1,35 @@
 package application;
 
+import java.util.ArrayList;
+
 public class Acti {
+
+    String[] rajz = {
+            "ködlámpa", "faág", "éjjeliszekrény", "gondolatolvasás", "esőkabát",
+            "túristajelzés", "madártoll", "téliálom", "fenyőillat",
+            "kenyérpirító", "családfa", "kutatócsoport", "rakott krumpli", "befőttesüveg",
+            "villámcsapás", "elnökválasztás", "vizipóló", "aranyeső", "gumilbda",
+            "madártej", "parikajancsi", "tőkehal", "dobverő", "fűszál", "csínytevő", "konyhanyelv"
+    };
+
+    String[] beszel = {
+            "közellenség", "tejszínhab", "mestermű", "csavarhúzó", "egyirányú utca",
+            "lakat", "satufék", "függöny karnis", "gondolatolvasó", "gyerekzár",
+            "ruháskosár", "bundás kenyér", "bakkecske", "gyapjúszőnyeg", "babágy",
+            "szelyemhernyó", "alamafa", "borbély", "tükörtojás", "elemlámpa",
+            "pásztorkutya", "széldzseki", "jobbkézszabály", "kerékpártároló",
+            "kilométeróra", "körjorgalom"
+    };
+
+    String[] mutogat = {
+            "kulcstartó", "tricikli", "lemezlovas", "szolgasors", "ütvefúró",
+            "szorítókötés", "önkritika", "fakír", "repülőtér", "költőpénz",
+            "gyerekjáték", "borospince", "számlatömb", "tűznyelő", "lótetű",
+            "fagyáspont", "télapó", "ádámkosztüm", "visszajátszás", "tojástartó",
+            "távcső", "családtag", "alagút", "jelszó", "szivargyujtó", "varródoboz",
+            "boroshordó"
+    };
+
 
 
 
